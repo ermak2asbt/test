7bab82e08a03e76cc1432f2563ff4233db422cb3
ermak <ermak_asbt@mail.ru>
2017-08-01 14:14:25 +0500

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello  World!");
+        System.out.println("Hello1  World!");
+        System.out.println("Hello2  World!");
     }
 }
